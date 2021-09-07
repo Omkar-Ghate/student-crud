1642ba3f5df3245011829dfa2ee3e906a91aa74b
Omkar-Ghate <otg0231@gmail.com>
2021-09-07 15:31:31 +0530

    chore: update navbar links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { Link, NavLink} from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
-        <Link className="navbar-brand" to="/">
-          Student
+        <Link className="navbar-brand" href="/">
+          Recat User
         </Link>
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" exact aria-current="page" to="/">
+              <NavLink className="nav-link" exact to="/">
                 Home
               </NavLink>
             </li>
@@ -38,6 +38,8 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+
+        <Link className="btn btn-outline-light" to="/users/add">Add User</Link>
       </div>
     </nav>
   );
