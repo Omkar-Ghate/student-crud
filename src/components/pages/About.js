b565da8c2f9160fa58acfe55f9ef0b715012c338
Omkar-Ghate <otg0231@gmail.com>
2021-09-07 15:34:53 +0530

    chore: update about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,20 +5,8 @@ const About = () => {
     <div className="container">
       <div className="py-4">
         <h1>About Page</h1>
-        <p className="lead">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
-          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
-          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
-          laborum dolorum libero maiores!
-        </p>
-
-        <p className="lead">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
-          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
-          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
-          laborum dolorum libero maiores!
-        </p>
-        <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt nam accusantium? Soluta cupiditate, accusamus commodi praesentium laborum dolorum libero maiores!</p>
+        <p className="lead">This is a student management app. You can view details of the student, edit the details of existing student, or delete any student details</p>
+      
       </div>
     </div>
   );
